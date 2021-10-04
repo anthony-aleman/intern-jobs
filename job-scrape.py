852cb9cdf15ab50128e6583e061f41b170466209
anthony-aleman <58735466+anthony-aleman@users.noreply.github.com>
2021-10-04 11:20:55 -0400

    Captured all job postings
    captured all the job postings for SWE intern inside variable job_postings

--- job-scrape.py
+++ job-scrape.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import pandas as pd
 from dotenv import load_dotenv
 import os
+import time
 
 # load environment variables
 load_dotenv()
@@ -11,7 +12,7 @@ WHERE=os.getenv('HOME')
 
 # configure the driver to use Chrome
 driver = webdriver.Chrome('C:\Windows\chromedriver\chromedriver')
-
+driver.implicitly_wait(3)
 
 # list to store job postiton titles
 positions = []
@@ -25,20 +26,37 @@ location = []
 # navigate to link
 driver.get('https://www.indeed.com/')
 
+
+
 # job entry
-job = driver.find_element_by_xpath('//*[@id="text-input-what"]').send_keys(INTERN)
-job.send_keys('\n')
+driver.find_element_by_xpath('//*[@id="text-input-what"]').send_keys(INTERN)
+
 
 # location entry
-driver.find_element_by_xpath('//*[@id="text-input-where"]')
+#driver.find_element_by_xpath('//*[@id="text-input-where"]')
+
 
 # search
-search = driver.find_element_by_xpath('//*[@id="jobsearch"]/button')
-search.click()
+# sometimes one xpath works and sometimes the other xpath works
+try:
+    driver.find_element_by_xpath('//*[@id="jobsearch"]/button').click()
+except Exception:
+    print(f'recieved exception: {Exception}')
+
+try:
+    driver.find_element_by_xpath('//*[@id="whatWhereFormId"]/div[3]/button').click()
+except Exception:
+    print(f'recieved exception: {Exception}')
+
 
 # store the page content
-content = driver.page_source
-soup = BeautifulSoup(content, features='html.parser')
+#content = driver.page_source
+#soup = BeautifulSoup(content, features='html.parser')
 
-for a in soup.find_all('a', attrs={'class': 'slider_container'}):
-    print(a)
+
+# finding all the job posting elements
+job_postings = driver.find_elements_by_class_name('job_seen_beacon')
+
+for job_card in job_postings:
+    print(job_card.text)
+    # TODO: scrape the name, location, salary and reviews if there are any
